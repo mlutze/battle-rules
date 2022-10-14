64a48d6e4c745ef37a903f3600c0edfd80d74b3f
Matthew Lutze <mlutze@users.noreply.github.com>
2022-10-14 15:24:10 +0200

    fix error

--- calc/calc.py
+++ calc/calc.py
@@ -21,14 +21,14 @@ def pickAByMaxHit(units: list[Unit], P: float, H: float, S: float, t: int) -> fl
     attacks = chain.from_iterable(unit.creature.attacks.values() for unit in units)
     max_attack: Attack = max(attacks, key=lambda a: attack_ratio(a, P))
     num = max_attack.damage * (10 + max_attack.bonus) ** P * H * S
-    denom = max_attack.creature.health * max_attack.creature.ac ** P * (S - M)
+    denom = max_attack.creature.health * max_attack.creature.ac ** P * (S - t)
     return num / denom
 
 def pickAByMinHit(units: list[Unit], P: float, H: float, S: float, t: int) -> float:
     attacks = chain.from_iterable(unit.creature.attacks.values() for unit in units)
     min_attack: Attack = min(attacks, key=lambda a: attack_ratio(a, P))
     num = min_attack.damage * (10 + min_attack.bonus) ** P * H * S
-    denom = min_attack.creature.health * min_attack.creature.ac ** P * (S - M)
+    denom = min_attack.creature.health * min_attack.creature.ac ** P * (S - t)
     return num / denom
 
 def health_factor(unit: Unit, P: float) -> float:
