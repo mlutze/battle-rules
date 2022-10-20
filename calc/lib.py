4764ae93b08321672b6a6fdd732728973e847fc6
Matthew Lutze <mlutze@users.noreply.github.com>
2022-10-20 14:43:28 +0200

    improve API

--- calc/lib.py
+++ calc/lib.py
@@ -1,49 +1,51 @@
 from models import *
 
-commoner = Creature("Commoner", Size.Medium, 4.5, 10, {})
-commoner.add_attack("Club", 2, 2.5)
+commoner = Creature("Commoner", Size.Medium, 4.5, 10) \
+    .with_attack("Club", 2, 2.5) \
 
-armored_commoner = Creature("Armored Commoner", Size.Medium, 4.5, 13, {})
-armored_commoner.add_attack("Club", 2, 2.5)
+armored_commoner = Creature("Armored Commoner", Size.Medium, 4.5, 13) \
+    .with_attack("Club", 2, 2.5) \
 
-orc = Creature("Orc", Size.Medium, 15, 13, {})
-orc.add_attack("Javelin", 5, 6.5)
-orc.add_attack("Greataxe", 5, 9.5)
+orc = Creature("Orc", Size.Medium, 15, 13) \
+    .with_attack("Javelin", 5, 6.5) \
+    .with_attack("Greataxe", 5, 9.5) \
 
-tarrasque = Creature("Tarrasque", Size.Gargantuan, 676, 25, {})
-tarrasque.add_attack("Bite", 19, 36)
-tarrasque.add_attack("Claw", 19, 28)
-tarrasque.add_attack("Horns", 19, 32)
-tarrasque.add_attack("Tail", 19, 24)
+tarrasque = Creature("Tarrasque", Size.Gargantuan, 676, 25) \
+    .with_attack("Bite", 19, 36) \
+    .with_attack("Claw", 19, 28) \
+    .with_attack("Horns", 19, 32) \
+    .with_attack("Tail", 19, 24) \
 
-frog = Creature("Frog", Size.Tiny, 1.5, 11, {})
+frog = Creature("Frog", Size.Tiny, 1.5, 11) \
 
-knight = Creature("Knight", Size.Medium, 52, 19, {})
-knight.add_attack("Greatsword", 5, 10)
-knight.add_attack("Heavy Crossbow", 2, 5.5)
-knight.add_attack("Multiattack", 5, 20)
+knight = Creature("Knight", Size.Medium, 52, 19) \
+    .with_attack("Greatsword", 5, 10) \
+    .with_attack("Heavy Crossbow", 2, 5.5) \
+    .with_attack("Multiattack", 5, 20) \
 
-bandit = Creature("Bandit", Size.Medium, 11, 12, {})
-bandit.add_attack("Scimitar", 3, 4.5)
-bandit.add_attack("Light Crossbow", 3, 5.5)
+bandit = Creature("Bandit", Size.Medium, 11, 12) \
+    .with_attack("Scimitar", 3, 4.5) \
+    .with_attack("Light Crossbow", 3, 5.5) \
 
-kobold = Creature("Kobold", Size.Small, 5, 12, {})
-kobold.add_attack("Dagger", 4, 4.5)
-kobold.add_attack("Sling", 4, 4.5)
+kobold = Creature("Kobold", Size.Small, 5, 12) \
+    .with_attack("Dagger", 4, 4.5) \
+    .with_attack("Sling", 4, 4.5) \
 
-dryad = Creature("Dryad", Size.Medium, 22.5, 16, {})
-dryad.add_attack("Club With Shillelagh", 6, 8.5)
+dryad = Creature("Dryad", Size.Medium, 22.5, 16) \
+    .with_attack("Club With Shillelagh", 6, 8.5) \
 
-harpy = Creature("Harpy", Size.Medium, 38.5, 11, {})
-harpy.add_attack("Claws", 3, 6)
-harpy.add_attack("Club", 3, 3.5)
-harpy.add_attack("Multiattack", 3, 9.5)
+harpy = Creature("Harpy", Size.Medium, 38.5, 11) \
+    .with_attack("Claws", 3, 6) \
+    .with_attack("Club", 3, 3.5) \
+    .with_attack("Multiattack", 3, 9.5) \
 
-bugbear = Creature("Bugbear", Size.Medium, 27.5, 16, {})
-bugbear.add_attack("Morningstar", 4, 11)
-bugbear.add_attack("Javelin", 4, 9)
+bugbear = Creature("Bugbear", Size.Medium, 27.5, 16) \
+    .with_attack("Morningstar", 4, 11) \
+    .with_attack("Javelin", 4, 9) \
 
-elephant = Creature("Elephant", Size.Huge, 76, 12, {})
-elephant.add_attack("Gore", 8, 19.5)
-elephant.add_attack("Stomp", 8, 19.5)
-elephant.add_attack("Trampling Charge", 8, 39)
+elephant = Creature("Elephant", Size.Huge, 76, 12) \
+    .with_attack("Gore", 8, 19.5) \
+    .with_attack("Stomp", 8, 19.5) \
+    .with_attack("Trampling Charge", 8, 39) \
+
+pass
--- calc/models.py
+++ calc/models.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple
+from dataclasses import dataclass, field
+from typing import NamedTuple, Optional
 from enum import Enum
 
 class Size(Enum):
@@ -15,22 +16,27 @@ class Attack(NamedTuple):
     damage: float
     creature: "Creature"
 
-class Creature(NamedTuple):
+@dataclass(frozen=True)
+class Creature:
     name: str
     size: Size
     health: float
     ac: int
-    attacks: dict[str, Attack]
-
-    def add_attack(self, name: str, bonus: int, damage: float) -> Attack:
-        attack = Attack(name, bonus, damage, self)
-        self.attacks[name] = attack
+    attacks: dict[str, Attack] = field(default_factory=dict)
 
     def __mul__(self, size: int) -> "Unit":
         return Unit(self, size)
 
     def __rmul__(self, size: int) -> "Unit":
         return Unit(self, size)
+    
+    def with_attack(self, name: str, bonus: int, damage: float) -> "Creature":
+        that = Creature(self.name, self.size, self.health, self.ac)
+        for att_name, att0 in self.attacks.items():
+            att = Attack(att0.name, att0.bonus, att0.damage, that)
+            that.attacks[att_name] = att
+        that.attacks[name] = Attack(name, bonus, damage, that)
+        return that
     
 class Unit(NamedTuple):
     creature: Creature
